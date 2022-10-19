0bf61e2134ba2eb6d8695ac4810dce337ec3c01c
UPO-JZSB <zgzhaoliang@gmail.com>
2022-10-19 16:35:33 +0800

    Create HelloGoodbye.java

---
+++ Week1.1 Course Introduction/HelloGoodbye.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        StdOut.println("Hello " + args[0] + " and " + args[1] + ".");
+        StdOut.println("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
